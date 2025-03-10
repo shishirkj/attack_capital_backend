626f4ee79a0f643bee32df2aa1cb53539527baf0
Shishir <90249481+shishirkj@users.noreply.github.com>
2025-03-10 22:41:34 +0530

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -10,7 +10,7 @@ import config from "./config/config";
 import { setupSwagger } from "./swagger";
 dotenv.config({ path: "./.env" });
 export const envMode = process.env.NODE_ENV?.trim() || "DEVELOPMENT";
-const port = config.port;
+const port = config.port||4000;
 const mongoURI = config.mongoURI || "mongodb://localhost:27017";
 connectDB(mongoURI);
 const app = express();
