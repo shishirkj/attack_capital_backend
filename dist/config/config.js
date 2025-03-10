c9e5dc2ff3ba991de551fef5f13cdee4608cf54b
Shishir <skjskjskj333@gmail.com>
2025-03-10 22:53:29 +0530

    build fix

--- dist/config/config.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-export default {
-    port: process.env.PORT || 5000,
-    mongoURI: process.env.MONGO_URI || "mongodb://localhost:27017/mydb",
-    jwtSecret: process.env.JWT_SECRET || "your_jwt_secret",
-    frontendUrl: process.env.FRONTEND_URL
-};
--- dist/validations/postValidation.js
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-export const postSchema = z.object({
-    title: z.string().min(1, { message: "Title must be at least 3 characters" }),
-    content: z
-        .string()
-        .min(1, { message: "Content must be at least 10 characters" }),
-});
